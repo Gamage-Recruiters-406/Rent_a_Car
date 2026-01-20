49daaadcf2e627f4d94eb6b4f44083e92570a555
Kavin <kavinmadusanka20011@gmail.com>
2026-01-20 13:44:19 +0530

    create signout function | Completed, get email verification part | Completed

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,8 +2,12 @@ import express from 'express';
 import {registerUser,
     SignIn,
     registerOwner,
-    verifyEmail
+    verifyEmail,
+    ReSendVerificationMail,
+    logout
 } from "../controllers/userController.js"
+
+import { requiredSignIn, isOwner } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
@@ -12,10 +16,15 @@ router.post("/register", registerUser);
 
 //login route
 router.post("/login", SignIn);
+//logout function
+router.post("/logout", requiredSignIn, logout);
 
 //rejister as a vehicle owner
 router.post("/OwnerRegistration", registerOwner);
 //vehicle owner verificaton route
 router.get("/verify-email", verifyEmail);
+
+//get verify email again
+router.patch("/getVerificationMail",requiredSignIn, isOwner, ReSendVerificationMail);
 
 export default router;
