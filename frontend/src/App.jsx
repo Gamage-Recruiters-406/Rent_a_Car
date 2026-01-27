79678c44f598094c2af0d83c33786c9436103e9e
oshivimarsha <oshadivimarsha@gmail.com>
2026-01-27 15:33:29 +0530

    Add font and colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
+import OwnerBookingRequest from "./pages/owner/OwnerBookingRequest";
+import "./App.css";
 function App() {
 
   return (
     <Router>
       <Routes>
         <Route path="/verify-email" element={<VerifyEmailPage/>} />
+        <Route path="/owner/booking-requests" element={<OwnerBookingRequest/>} />
       </Routes>
     </Router>
   )
