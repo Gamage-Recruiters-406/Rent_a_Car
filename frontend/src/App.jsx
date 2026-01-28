5a992063a40cde7f3f1e77b73dd985102603d86f
Chamika Jayan <jayanchamika274@gmail.com>
2026-01-28 09:28:00 +0530

    Add customer vehicle details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
-function App() {
+import VehicleDetailsPage from "./pages/VehicleDetailsPage";
 
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/verify-email" element={<VerifyEmailPage/>} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+        <Route path="/vehicles/:id" element={<VehicleDetailsPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
