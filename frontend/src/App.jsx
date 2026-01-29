bbab5b0893d89815ced83d04a952beee11f570fa
Chamika Jayan <jayanchamika274@gmail.com>
2026-01-29 17:17:42 +0530

    comment booking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,14 @@ import { Toaster } from "react-hot-toast";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
 import VehicleDetailsPage from "./pages/VehicleDetailsPage";
 import { SignInPage } from "./pages/login/SignInPage";
-import{SignUpPage} from "./pages/login/SignupPage";
-import { HomePage } from "./pages/HomePage";      
+import { SignUpPage } from "./pages/login/SignupPage";
+import { HomePage } from "./pages/HomePage";
 import { CustomerVehicleListPage } from "./pages/CustomerVehicleListPage";
 import ContactPage from "./pages/ContactPage";
 import { ForgotPasswordPage } from "./pages/login/forgotpassword/ForgotPasswordPage";
 import { VerifyCodePage } from "./pages/login/forgotpassword/VerifyCodePage";
 import { ResetPasswordPage } from "./pages/login/forgotpassword/ResetPasswordPage";
 import CustomerReviews from "./pages/CustomerRating";
-
-
 
 function App() {
   return (
@@ -25,21 +23,19 @@ function App() {
         <Route path="/" element={<CustomerVehicleListPage />} />
         <Route path="/vehicles" element={<CustomerVehicleListPage />} />
         <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-        <Route path="/contact" element={<ContactPage/>} />
-        <Route path="/customer-reviews" element={<CustomerReviews/>} />
-        <Route path="/admin/booking" element={<AdminBooking/>} />
-
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/customer-reviews" element={<CustomerReviews />} />
+        {/*<Route path="/admin/booking" element={<AdminBooking />} />*/}
 
         {/* login */}
 
-        <Route path="/login" element={<SignInPage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
-        <Route path="/verify-email" element={<VerifyEmailPage/>} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
 
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/verify-code" element={<VerifyCodePage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
-
       </Routes>
     </Router>
   );
