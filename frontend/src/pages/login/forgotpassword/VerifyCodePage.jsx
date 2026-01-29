c66285bd657edd216627572528a5f26b0adf3408
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2026-01-29 10:35:07 +0530

    Fix sign up link and update OTP verification request
    nt and payload accordingly.

--- frontend/src/pages/login/forgotpassword/VerifyCodePage.jsx
+++ frontend/src/pages/login/forgotpassword/VerifyCodePage.jsx
@@ -37,7 +37,8 @@ export function VerifyCodePage() {
     }
     setIsLoading(true);
     try {
-      const response = await axios.post(`${baseUrl}${apiVersion}/authUser/verifyResetOtp`, { email, otp: code });
+      const response = await axios.get(`${baseUrl}${apiVersion}/authUser/verifyOTP`,
+         { email, otp: code });
       if(response.status === 200){
         toast.success('Verification code verified successfully');
       }
