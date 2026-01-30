77379f24a18cbdd24d4af158abfb9419d6d39939
Mayomibhagya <mayomibhagya.mb@gmail.com>
2026-01-30 09:48:21 +0530

    ADD vehicle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
+import VehicleDetailsPage from "./pages/VehicleDetailsPage";
+import { SignInPage } from "./pages/login/SignInPage";
+import{SignUpPage} from "./pages/login/SignupPage";
+import { CustomerVehicleListPage } from "./pages/CustomerVehicleListPage";
+import AddVehicle from "./pages/AddVehicle";
+
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/verify-email" element={<VerifyEmailPage/>} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
+        <Route path="/vehicles/:id" element={<VehicleDetailsPage />} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<CustomerVehicleListPage />} />
+        <Route path="/vehicles" element={<CustomerVehicleListPage />} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        <Route path="/add-vehicle" element={<AddVehicle />} />
+
+        {/* login */}
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/verify-email" element={<VerifyEmailPage />} />
       </Routes>
     </Router>
   )
