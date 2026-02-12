84788cf20f4944e4903ef6ba1e963105f021ddba
Jeyson Miranda <jeysonmiranda64@gmail.com>
2026-02-12 16:26:10 +0530

    called post API for review post

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,6 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
     <View className="flex-1 justify-center items-center bg-white">
       <Text className="text-2xl font-bold text-center text-blue-500">
@@ -9,6 +11,13 @@ export default function HomeScreen() {
       <Text className="text-lg text-green-600 mt-4">
         Tailwind is running
       </Text>
+
+      <TouchableOpacity
+        className="mt-6 bg-[#0D3778] px-6 py-3 rounded-xl"
+        onPress={() => router.push('/Reviews')}
+      >
+        <Text className="text-white font-semibold">Go to Reviews</Text>
+      </TouchableOpacity>
     </View>
   );
 }
