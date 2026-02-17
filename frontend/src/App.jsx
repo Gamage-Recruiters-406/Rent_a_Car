1d76b33e3279539b3175a8c48cfcd43d9f84657a
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2026-02-17 12:20:11 +0530

    Update Notification Part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ import VehicleManagement from "./pages/admin/VehicleManagement";
 import AdminSettingsPage from "./pages/admin/AdminSettingsPage.jsx";
 import AdminNotifications from "./pages/admin/AdminNotifications";
 import OwnerBookingRequests from "./pages/owner/OwnerBookingRequest.jsx";
-import AdminNotifications from "./pages/admin/AdminNotifications.jsx";
+
 
 
 function App() {
