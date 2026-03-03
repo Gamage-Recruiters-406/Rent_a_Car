8af9a0b16c39611f6507dd0b10ca3413d1f8cd52
Piyumal <pasindupiumalcrw2002@gmail.com>
2026-03-03 11:46:56 +0530

    admin get all payments and owner get their payments

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,6 +1,9 @@
 import express from 'express';
-import { isCustomer, requiredSignIn } from '../middlewares/authMiddleware.js';
-import { createPayment, getPaymentsByUserId } from '../controllers/paymentController.js';
+import { isCustomer,isOwner,isAdmin, requiredSignIn } from '../middlewares/authMiddleware.js';
+import { createPayment, 
+         getPaymentsByUserId,
+         getPaidPaymentsForOwner,
+         getAllPaidPaymentsForAdmin } from '../controllers/paymentController.js';
 
 const router = express.Router();
 
@@ -8,5 +11,12 @@ router.post('/create-payment',requiredSignIn, createPayment );
 // router.post('/webhook', express.raw({type: 'application/json'}), stripeWebhook );
 
 router.get("/get-payments", requiredSignIn, isCustomer, getPaymentsByUserId);
+
+//owner views payments received for their vehicles
+router.get("/get-paid-payments-for-owner", requiredSignIn,isOwner, getPaidPaymentsForOwner);
+
+//admin views all paid payments across the platform
+router.get("/get-all-paid-payments-for-admin", requiredSignIn,isAdmin, getAllPaidPaymentsForAdmin);
+
 
 export default router;
